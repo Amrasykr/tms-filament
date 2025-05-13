d36ee13511e04cc1847f469a5cb72bff4527d08c
Ammar Asysyakur <asysyakurammar125@gmail.com>
2025-05-13 13:22:15 +0700

    update: snyc task score while task deleted

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -31,7 +31,28 @@ class Task extends Model
         static::created(function (Task $task) {
             $task->generateStudentTasks();
         });
+
+        static::deleted(function ($task) {
+            $scheduleId = $task->schedule_id;
+
+            // Ambil semua student dari kelas terkait
+            $studentIds = \App\Models\StudentClass::where('class_id', $task->schedule->class_id)->pluck('student_id');
+
+            foreach ($studentIds as $studentId) {
+                // Hitung ulang task_score untuk setiap siswa
+                $averageScore = \App\Models\StudentTask::whereHas('task', function ($query) use ($scheduleId) {
+                    $query->where('schedule_id', $scheduleId);
+                })->where('student_id', $studentId)->avg('score') ?? 0;
+
+                // Update nilai task_score di tabel grades
+                \App\Models\Grade::where('student_id', $studentId)
+                    ->where('schedule_id', $scheduleId)
+                    ->update(['task_score' => $averageScore]);
+            }
+        });
     }
+
+    
 
     public function generateStudentTasks()
     {
